5212c7428ca29814e7a6d0b514e423fb6aeec07f
Andrea055 <vivanco11.ac@gmail.com>
2022-08-10 20:24:53 +0200

    add new crate to add new application and update repo

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is the repository of WOL, containing all official apps supported by WOL.
 If you want the support for a new application open an issue or create the script then add its to this repository
+
+To add a new application to repository run script by execute cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::env;
+use std::io::{stdin, Write};
 use std::fs;
+use sargparse::ArgumentParser;
+use serde_json::{Value};
+use serde_json::json;
 
 fn main() {
 
@@ -8,5 +11,64 @@ fn main() {
     let contents = fs::read_to_string("./repository.json")
         .expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    let mut jsonstring: Value = serde_json::from_str(&contents).unwrap();
+    let mut jsonarray = jsonstring.as_array_mut().unwrap();
+    let mut appname = String::new();
+    println!("Please enter the name of the app that you want to add");
+    stdin().read_line(&mut appname)
+    	.ok()
+        .expect("Failed to read line");
+    let mut cliname = String::new();
+    println!("Please enter the name that your app will have in Wineget CLI");
+    stdin().read_line(&mut cliname)
+        .ok()
+        .expect("Failed to read line");
+    println!("Now enter the description of your application");
+    let mut description = String::new();
+    stdin().read_line(&mut description)
+        .ok()
+        .expect("Failed to read line");
+    let mut repository = String::new();
+    println!("Enter the url of GIT repository where your script live");
+    stdin().read_line(&mut repository)
+        .ok()
+        .expect("Failed to read line");
+    let mut path = String::new();
+    println!("Please enter the path where your script install the Wine application");
+    stdin().read_line(&mut path)
+        .ok()
+        .expect("Failed to read line");
+    let mut icon = String::new();
+    println!("Please enter the URL of icon of Application");
+    stdin().read_line(&mut icon)
+        .ok()
+        .expect("Failed to read line");
+    let mut author = String::new();
+    println!("Write the author of script");
+    stdin().read_line(&mut author)
+        .ok()
+        .expect("Failed to read line");
+    let mut version = String::new();
+    println!("Write the version of script");
+    stdin().read_line(&mut version)
+        .ok()
+        .expect("Failed to read line");
+    jsonarray.push(json!(
+        {
+            "App": appname.replace("\n", ""),
+            "Cliname": cliname.replace("\n", ""),
+            "Description": description.replace("\n", ""),
+            "Repository": repository.replace("\n", ""),
+            "Path": path.replace("\n", ""),
+            "Icon": icon.replace("\n", ""),
+            "Author": author.replace("\n", ""),
+            "Version": version.replace("\n", "")
+        }
+    ));
+    
+    let mut repofile = std::fs::OpenOptions::new().write(true).open("./repository.json").unwrap();
+    repofile.write_all(serde_json::to_string_pretty(jsonarray).unwrap().as_bytes()).unwrap();
+    repofile.flush().unwrap();
+    //serde_json::to_writer(repofile, jsonarray).unwrap();
+
 }
